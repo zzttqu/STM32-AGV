b8e3834b76cdeae31c278df8e0e33676603babe0
zzttqu <zzttqu@gmail.com>
2023-03-12 12:44:58 +0800

    修改了电机引脚

--- Core/Inc/motor.h
+++ Core/Inc/motor.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: zzttqu zzttqu@gmail.com
+ * @Date: 2023-02-25 23:05:12
+ * @LastEditors: zzttqu zzttqu@gmail.com
+ * @LastEditTime: 2023-03-12 12:44:15
+ * @FilePath: \uart\Core\Inc\motor.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include "main.h"
 #include "tim.h"
 #define pai 3.14159
@@ -14,13 +22,13 @@
 #define MOTORC_FORWARD HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET)
 #define MOTORC_BACKWARD HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET)
 
-#define MOTORD_FORWARD HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET)
-#define MOTORD_BACKWARD HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET)
+#define MOTORD_FORWARD HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_SET)
+#define MOTORD_BACKWARD HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_RESET)
 
 #define MOTORA_SPEED HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_4)
 #define MOTORB_SPEED HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_4)
 #define MOTORC_SPEED HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0)
-#define MOTORD_SPEED HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_2)
+#define MOTORD_SPEED HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_10)
 typedef struct _Motor_Parameter_
 {
   float target;
